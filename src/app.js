536e8fcde406d48e00dcda3665af4093cc76c77f
Franco Caraballo <franconcaraballo@gmail.com>
2025-04-08 21:21:34 -0300

    rename files

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import mocksRouter from './routes/mocks.routes.js'
-import petsRouter from './routes/pets.routes.js'
+import dataRouter from './routes/data.routes.js'
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/mocks', mocksRouter);
-app.use('/pets', petsRouter);
+app.use('/data', dataRouter);
 
 app.listen('8080', () => {
     console.log("Server on");
